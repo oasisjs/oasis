d1bd7cd80e9729825fe05da7ac1336567b10784c
Yuzu <yuzuru@programmer.net>
2022-07-20 20:21:48 -0500

    fix: reflect metadata compat

--- deps.ts
+++ deps.ts
@@ -1,6 +1,4 @@
 export * from 'https://x.nest.land/biscuit@0.2.1/packages/biscuit/mod.ts';
 export * from 'https://x.nest.land/biscuit@0.2.1/packages/discordeno/mod.ts';
 export type { Events } from 'https://x.nest.land/biscuit@0.2.1/packages/biscuit/Actions.ts';
-export { Reflect } from "https://deno.land/x/deno_reflect@v0.2.1/mod.ts";
-
-globalThis.Reflect = Reflect;
+export type { Reflect } from "https://deno.land/x/deno_reflect/mod.ts";
---
+++ reflect-metadata.ts
@@ -0,0 +1,3 @@
+import { Reflect } from "https://deno.land/x/deno_reflect/mod.ts";
+
+globalThis.Reflect = Reflect;
