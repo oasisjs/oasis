fd463859f2aee7045f424bd721f76b87a7902026
Yuzu <yuzuru@programmer.net>
2022-07-20 21:47:26 -0500

    fix: fmt

--- tests/mod.ts
+++ tests/mod.ts
@@ -1,10 +1,5 @@
 import 'https://deno.land/std@0.146.0/dotenv/load.ts';
-import {
-    GatewayIntents,
-    loadDirs as handle,
-    modules,
-    Session
-} from './deps.ts';
+import { GatewayIntents, loadDirs as handle, modules, Session } from './deps.ts';
 
 const intents = GatewayIntents.Guilds | GatewayIntents.GuildMessages | GatewayIntents.MessageContent;
 const session = modules.enableCommandContext('!')(new Session({ token: Deno.env.get('TOKEN')!, intents }));
